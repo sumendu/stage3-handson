3caa4fffe1b2644a8092e8ef8e435797150acb8c
sumendu <81095743+sumendu@users.noreply.github.com>
2021-03-26 20:18:10 +0530

    Add files via upload

---
+++ Reacthandson-1/myfirstreact/src/App.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+class App extends Component
+{
+  render()
+  {
+    return(
+      <center>
+        <h1>Welcome the first session of react</h1>
+      </center>
+    )
+  }
+}
+export default App
